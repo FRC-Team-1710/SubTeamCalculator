8a337105ce9c137c4ef2399611fd9f418873e157
Charlie C. Bergdall <779ccb24@stu.olatheschools.org>
2020-11-10 19:37:02 -0600

    Outro backbone (not working yet)

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,23 @@
 public class MyClass {
     public static void main(String args[]) {
+        outro();
+    }
+    
+    public static void outro(boolean finalLeaders, boolean initialLeaders, int [] array) {
+        if(initialLeaders) {
+            String initialLeaderString = "Including leaders in the original count";
+        } else {
+            String initialLeaderString = "Excluding leaders in the original count";
+        }
+
+        if(finalLeaders) {
+            String finalLeaderString = "including leaders in the final count,";
+        } else {
+            String finalLeaderString = "excluding leaders in the final count,";
+        }
+
+        System.out.println(initialLeaderString + " and \n" + finalLeaderString + " there \n will be " + programmerCount + " programmers, " + builderCount + " builders,\n " + designerCount + " designers, " + 
+        marketerCount + " marketers, " + outreacherCount + " outreachers,\n " + initiativerCount + " initiativers, " + mediaerCount + " mediaers, " + graphicerCount + " graphicers,\n and " + financerCount + " financers.");
 
     }
-	
 }
