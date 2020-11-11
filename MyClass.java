e2cd03c48769bd4b3d2e908758ab7ff436d2f57a
Cooper S. Wright <726csw12@stu.olatheschools.org>
2020-11-10 19:20:56 -0600

    Question1

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,16 @@
-public class MyClass {
-    public static void main(String args[]) {
+import java.util.Scanner; //imports scanner class
 
-    }
+public class MyClass {
+	public static void main(String args[]) {
+	   
+	}
+	   public static int pot(){
+	     Scanner potScanner = new Scanner(System.in);
+	     System.out.println("How many people are on the team?");
+	     //"pot" means people on team
+	     int pot = potScanner.nextInt();
+	     return pot;
+	    }
 	
+
 }
